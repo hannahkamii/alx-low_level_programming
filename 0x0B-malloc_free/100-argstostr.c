ea774bdd4b8d66d1c7a981fc30885ebef81bdf55
hannahkamii <hannahkaminja32@gmail.com>
2023-06-13 08:18:53 -0700

    malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: str
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, n, r = 0, l = 0;
+	char *p;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			l++;
+	}
+	l += ac;
+	p = malloc(sizeof(char) * l + 1);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+	{
+		p[r] = av[i][n];
+		r++;
+	}
+	if (p[r] == '\0')
+	{
+		p[r++] = '\n';	
+	}
+	return (p);
+	}
+	return (0);
+}
