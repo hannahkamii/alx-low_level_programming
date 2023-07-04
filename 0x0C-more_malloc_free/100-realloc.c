f6cc227c3ed1bfce86008a5ba46b020fc18d7e19
hannahkamii <hannahkaminja32@gmail.com>
2023-07-04 03:05:44 -0700

    haanah

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,42 +9,44 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-        char *p;
-        char *old_ptr;
-        unsigned int i;
-        if (new_size == old_size)
-                return (ptr);
-        if (new_size == 0 && ptr)
-        {
-                free(ptr);
-                return (NULL);
-        }
-        if (!ptr)
-                return (malloc(new_size));
-        p = malloc(new_size);
-        if (!p)
-                return (NULL);
-        old_ptr = ptr;
-        if (new_size < old_size)
-        {         free(ptr);
-                return (NULL);
-        }
-        if (!ptr)
-                return (malloc(new_size));
-        p = malloc(new_size);
-        if (!p)
-                return (NULL);
-        old_ptr = ptr;
-        if (new_size < old_size)
-        {
-                for (i = 0; i < new_size; i++)
-                        p[i] = old_ptr[i];
-        }
-        if (new_size > old_size)
-        {
-                for (i = 0; i < old_size; i++)
-                        p[i] = old_ptr[i];
-        }
-        free(ptr);
-        return (p);
+	char *p;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+		return (malloc(new_size));
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+	old_ptr = ptr;
+	if (new_size < old_size)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+		return (malloc(new_size));
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+	old_ptr = ptr;
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+		p[i] = old_ptr[i];
+	}
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			p[i] = old_ptr[i];
+	}
+	free(ptr);
+	return (p);
 }
