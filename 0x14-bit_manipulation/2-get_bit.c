65251cc12d97264bbee7e0a1676b4b543628e98e
hannahkamii <hannahkaminja32@gmail.com>
2023-07-06 07:16:50 -0700

    hannah is authentic

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @n: number of search
+ * @index: index of the bit
+ * Return: value of the bit
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit_val;
