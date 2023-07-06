7efbe75300e477df93ab4b54dd7194aa97399e5a
hannahkamii <hannahkaminja32@gmail.com>
2023-07-06 03:41:26 -0700

    hannah made it

---
+++ 0-binary_to_uint.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (!b)
+		return (0);
+	for (i = 0 ; b[i] ; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		result = 2 * result + (b[i] - '0');
+	}
+	return (result);
+}
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ */
+int main(int argc _attribute_((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
