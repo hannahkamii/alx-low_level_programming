7c88ec7796f37d6c11865ddddc1a35ddba122922
hannahkamii <hannahwacheke520@gmail.com>
2023-08-21 02:46:55 -0700

    hannah

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
-#!/bin/bass
-gcc -Wall -pedantic -Werror -Wextra -c -fPIC *.c
+#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
